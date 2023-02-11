3075ab2732c2ce4269ca6d0353c679b5c2ff33f0
Javier Garbalena <112207218+JavGarb@users.noreply.github.com>
2023-02-10 23:40:42 -0500

    Update membership_trainer.js

--- api/src/models/membership_trainer.js
+++ api/src/models/membership_trainer.js
@@ -2,7 +2,7 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "activities",
+    "membership_trainer",
     {
       id: {
         type: DataTypes.INTEGER,
