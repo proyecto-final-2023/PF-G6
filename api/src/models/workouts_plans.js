81e074bd8870a57857346901cb0534f5e65942ba
Javier Garbalena <112207218+JavGarb@users.noreply.github.com>
2023-02-10 23:41:35 -0500

    Update workouts_plans.js

--- api/src/models/workouts_plans.js
+++ api/src/models/workouts_plans.js
@@ -2,7 +2,7 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "activity",
+    "workouts_plans",
     {
       id: {
         type: DataTypes.UUID,
