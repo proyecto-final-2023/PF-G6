6987fd76f2ba72fae4c5db46f38316c7a6558a82
Hachikoi-the-creator <facka72@gmail.com>
2023-02-14 14:42:57 -0600

    checkpoint

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // external images config
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 module.exports = nextConfig;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,51 @@
-import React from "react";
+import CarouselItem from "@/components/CarouselItem";
+import React, { useState } from "react";
+import landindSlides from "../assets/landing-slides.json";
 //! THIS IS LANDING
 
-export default function Index() {
-  return <div>index</div>;
+export default function index() {
+  // TODO: open and close modal
+  const [slideNum, setSlideNum] = useState(0);
+  // const [modalOpen, setModalOpen] = useState(false);
+
+  // const handleOpenModal = (index: number) => {
+  //   setSlideNum(index);
+  //   setModalOpen(true);
+  // };
+
+  // const handleCloseModal = () => {
+  //   setModalOpen(false);
+  // };
+
+  const changeImgHandler = (direction: "next" | "prev") => {
+    let newIndex: number;
+
+    if (direction === "next") {
+      const willBeGreater = slideNum + 1 >= landindSlides.length;
+      newIndex = willBeGreater ? 0 : slideNum + 1;
+    }
+    //
+    else {
+      const willBeLower = slideNum - 1 < 0;
+      newIndex = willBeLower ? landindSlides.length - 1 : slideNum - 1;
+    }
+
+    setSlideNum(newIndex);
+  };
+
+  return (
+    <div className="galleryWrap">
+      {/* className="single" */}
+      <CarouselItem
+        imgData={landindSlides[slideNum]}
+        isModalOpen={true}
+        {...{ changeImgHandler }}
+      />
+      {/* <br />
+    Current slide number:  {slideNumber}
+    <br />
+    Total Slides: {landindSlides.length}
+    <br /><br /> */}
+    </div>
+  );
 }
