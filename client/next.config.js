f3906f50ec6a464afe116d8c5b99085c36b36415
Hachikoi-the-creator <facka72@gmail.com>
2023-02-16 20:54:13 -0600

    some changes

--- client/next.config.js
+++ client/next.config.js
@@ -2,18 +2,24 @@
 
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'graph.facebook.com'
-      
-    },{
-      protocol:'https',
-      hostname:'lh3.googleusercontent.com'
-    }
-  ],
-    
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+
+  i18n: {
+    locales: ["en-US", "es-ES"],
+    defaultLocale: "en-US",
+  },
 };
 
 module.exports = nextConfig;
