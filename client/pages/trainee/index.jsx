fc2ff3a6642675d7b39d38cd7a00ad1c4d6c2ae2
OssFit <oalatrista@gmail.com>
2023-02-17 13:06:42 -0500

    actualizando

---
+++ client/pages/trainee/index.jsx
@@ -0,0 +1,13 @@
+import {React,useState} from "react";
+import Image from "next/image";
+
+
+export default function Index() {
+  const[progress,setProgress]=useState("0")
+
+  return(
+  <div>
+    <h1>Progress:{progress}%</h1>
+  
+    </div>)
+}
