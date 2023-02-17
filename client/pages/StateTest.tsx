9c566f1455b8a3bb98c897c5da6d1bde5b4925f9
Hachikoi-the-creator <facka72@gmail.com>
2023-02-17 12:17:15 -0600

    fixed ts config

--- client/pages/StateTest.tsx
+++ client/pages/StateTest.tsx
@@ -2,22 +2,13 @@ import React from "react";
 import useStore from "@/store";
 
 export default function App() {
-  const { bears, fishes, post, fetchPost } = useStore((state) => ({
-    bears: state.bears,
-    fishes: state.fishes,
+  const { post, fetchPost } = useStore((state) => ({
     post: state.post,
     fetchPost: state.fetchPost,
   }));
 
   return (
     <main>
-      <h2>
-        Bears: <strong>{bears}</strong>
-      </h2>
-      <h2>
-        Fish: <strong>{fishes}</strong>
-      </h2>
-
       <button onClick={fetchPost}>Fetch stuff</button>
 
       <div>{post.id}</div>
