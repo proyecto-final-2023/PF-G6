404667cc4aa8ce084ef29941a64063f9075d4b6e
OssFit <oalatrista@gmail.com>
2023-02-18 00:23:03 -0500

    actualizando

--- client/pages/trainee/index.jsx
+++ client/pages/trainee/index.jsx
@@ -9,6 +9,11 @@ export default function Index() {
   return(
   <div>
     <h1>Progress:{progress}%</h1>
+    <Image/>Foto
+    <h2>Trainer Name</h2>
+    <h3>Today's Food Plan</h3>
+    <h3>Today's Excercise Plan</h3>
+    <h3>CRONOGRAMA SEMANAL</h3>
   
     </div>)
 }
