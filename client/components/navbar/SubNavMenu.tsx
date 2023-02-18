ff7361d7172e8acc2b32bd5fbb1d32cdebd16a16
TeoG1menez <teo.unr.gimenez@gmail.com>
2023-02-18 19:31:01 -0300

    fixed user Nav

--- client/components/navbar/SubNavMenu.tsx
+++ client/components/navbar/SubNavMenu.tsx
@@ -30,7 +30,7 @@ export default function SubNavMenu(props: SubNavMenuProps): ReturnVoidOrJsx {
   }
 
   return (
-    <ul className="absolute left-0 w-[115px] flex flex-col bg-gray-600 border-gray-200 rounded-lg shadow-lg transition ease-in-out duration-500">
+    <ul className="absolute left-0 w-[115px] flex flex-col bg-gray-600 border-gray-200 rounded-lg shadow-lg ease-in-out duration-500">
       {optionsList.map((option) => {
         const urlKey = option.replace(" ", "").toLowerCase() || "diets";
         // TS needed this to be happy, since I modifed the key before using it
@@ -39,7 +39,7 @@ export default function SubNavMenu(props: SubNavMenuProps): ReturnVoidOrJsx {
         return (
           <li key={option}>
             <Link href={url}>
-            <button type="button" className="inline-block px-6 py-2.5 bg-transparent font-medium text-xs leading-tight uppercase hover:bg-gray-600 hover:text-orange-500 rounded w-[115px] transition duration-300 ease-in-out">{option}</button>
+            <button type="button" className="inline-block px-6 py-2.5 bg-transparent font-medium text-xs leading-tight uppercase hover:bg-gray-600 hover:text-orange-500 w-[115px] rounded-lg ease-in-out duration-300">{option}</button>
             </Link>
           </li>
         );
