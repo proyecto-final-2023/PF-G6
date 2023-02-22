d28bba1cc794baac4b7086eb438d12c1819f229e
Andrew <andres.velata@hotmail.com>
2023-02-22 18:36:55 -0500

    dos minusculas perdidas

--- api/src/models/PlanTrainer.js
+++ api/src/models/PlanTrainer.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "PlanTrainer",
+    "planTrainer",
     {
       id_planTrainer: {
         type: DataTypes.UUID,
