1a1e4ce593e18ba1506826a103311487686ebd7b
Juan Pablo Quinteros <jp.quinterosaraos@gmail.com>
2023-02-22 21:55:26 -0300

    css tools listo, faltan solo delles

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -33,61 +33,87 @@ console.log(user)
 
   return (
     <div>
-    <nav className="w-full bg-gray-800  p-0 h-[72px] border-x-none border-b-[2px]  border-yellow-900" >
-      <ul className="flex justify-around align-middle">
-        <li className="inline-block align-bottom text-center w-[100px] h-[65px]">
-        <Link replace href="/" scroll>
-        <Image src={logoImg} alt={`link of the whole app`} className="inline-block align-bottom mt-[3px] w-[100px] h-[65px]" />
-        </Link>
-        </li>
-        <li  className="inline-block align-bottom text-center pt-5">
-          {/* <input type="text" ref={searchRef} /> */}
-          <input type="search"  ref={searchRef} id="default-search" className="inline-block w-[150px] p-1 bg-gray-600 focus:bg-gray-500 focus:outline-none focus:w-[300px] duration-300 border-[2px] border-gray-400 rounded-l-lg  placeholder-white text-white" placeholder="Search..." required/>
-          <button type="submit" className='absolute p-2 inline-block bg-gray-600 border-[2px] border-gray-400 rounded-r-lg uppercase text-xs font-medium'>Search</button>
-    
-        </li>
+      <nav className="w-full bg-gray-800  p-0 h-[72px] border-x-none border-b-[2px]  border-yellow-900">
+        <ul className="flex justify-around align-middle">
+          <li className="inline-block align-bottom text-center w-[100px] h-[65px]">
+            <Link replace href="/" scroll>
+              <Image
+                src={logoImg}
+                alt={`link of the whole app`}
+                className="inline-block align-bottom mt-[3px] w-[100px] h-[65px]"
+              />
+            </Link>
+          </li>
+          <li className="inline-block align-bottom text-center pt-5">
+            {/* <input type="text" ref={searchRef} /> */}
+            <input
+              type="search"
+              ref={searchRef}
+              id="default-search"
+              className="inline-block w-[150px] p-1 bg-gray-600 focus:bg-gray-500 focus:outline-none focus:w-[300px] duration-300 border-[2px] border-gray-400 rounded-l-lg  placeholder-white text-white"
+              placeholder="Search..."
+              required
+            />
+            <button
+              type="submit"
+              className="absolute p-2 inline-block bg-gray-600 border-[2px] border-gray-400 rounded-r-lg uppercase text-xs font-medium"
+            >
+              Search
+            </button>
+          </li>
 
-        {/* sorthand for hoverEventHandler = {hoverEventHandler}*/}
-        
+          {/* sorthand for hoverEventHandler = {hoverEventHandler}*/}
 
-        <li className="inline-block align-bottom h-[70px] w-[115px] text-center">
-          <Link replace href="/home">
-          <button type="button" className="inline-block px-6  font-medium text-xs leading-tight h-[70px] w-[115px] uppercase rounded  hover:text-orange-500 transition duration-300 ease-in-out">Home</button>
-          </Link>
-        </li>
+          <li className="inline-block align-bottom h-[70px] w-[115px] text-center">
+            <Link replace href="/home">
+              <button
+                type="button"
+                className="inline-block px-6  font-medium text-xs leading-tight h-[70px] w-[115px] uppercase rounded  hover:text-orange-500 transition duration-300 ease-in-out"
+              >
+                Home
+              </button>
+            </Link>
+          </li>
 
-        <li className="inline-block align-bottom text-center w-[115px] h-full">      
-          <Link replace href="/guest/trainning-list">
-          <button type="button" className="inline-block px-6  font-medium text-xs leading-tight h-[70px] w-[115px] uppercase rounded hover:text-orange-500 transition duration-300 ease-in-out">Trainings</button>
-          </Link>
-        </li>
-       {/* <Image src={photo} alt="hola"></Image> */}
-       
+          <li className="inline-block align-bottom text-center w-[115px] h-full">
+            <Link replace href="/guest/trainning-list">
+              <button
+                type="button"
+                className="inline-block px-6  font-medium text-xs leading-tight h-[70px] w-[115px] uppercase rounded hover:text-orange-500 transition duration-300 ease-in-out"
+              >
+                Trainings
+              </button>
+            </Link>
+          </li>
+          {/* <Image src={photo} alt="hola"></Image> */}
 
-        <HoverLi
-          href="trainee/tool/tools"
-          text="tools"
-          isHover={hovers.tools}
-          optionsList={["Stop Watch", "Calories Calculator", "Fat Calculator"]}
-          {...{ hoverEventHandler }}
-        />
+          <HoverLi
+            href="trainee/tool/tools"
+            text="tools"
+            isHover={hovers.tools}
+            optionsList={[
+              "Stop Watch",
+              "Calories Calculator",
+              "Fat Calculator",
+            ]}
+            {...{ hoverEventHandler }}
+          />
 
-      {user?(<li className="m-5">Hello {name}</li>):console.log("")}
-        <HoverLi
-          imgUrl={userImg}
-          text="user"
-          href="/"
-          isHover={hovers.user}
-          optionsList={
-            user
-              ? ["Diets", "Trainer Programs", "Log out"]
-              : ["Register", "Log In"]
-          }
-          {...{ hoverEventHandler }}
-        />
-        
-      </ul>
-    </nav>
+          {user && <li className="m-5">Hello {name}</li>}
+          <HoverLi
+            imgUrl={userImg}
+            text="user"
+            href="/"
+            isHover={hovers.user}
+            optionsList={
+              user
+                ? ["Diets", "Trainer Programs", "Log out"]
+                : ["Register", "Log In"]
+            }
+            {...{ hoverEventHandler }}
+          />
+        </ul>
+      </nav>
     </div>
   );
 }
--- client/pages/trainee/tools/fat-calculator.tsx
+++ client/pages/trainee/tools/fat-calculator.tsx
@@ -9,13 +9,9 @@ const CalculateFatPercentage = () => {
   const [height, setHeight] = useState("");
   const [gender, setGender] = useState("male");
   const [result, setResult] = useState(0);
- 
-  
-  
- 
+
   const calculate = () => {
     let bodyFatPercentage = 0;
-  
 
     if (gender === "male") {
       bodyFatPercentage =
@@ -29,80 +25,87 @@ const CalculateFatPercentage = () => {
         78.387;
     }
 
-    setResult(bodyFatPercentage);
-    
-  
-    
+    setResult(Math.round(bodyFatPercentage));
   };
 
   return (
-    <div className="h-[82.5vh] bg-[url('/bgs/imgCalculator.png')] bg-no-repeat bg-cover backdrop-blur-sm">
-      <div className="w-[32vw] m-auto">
-       <div className="flex-col justify-items-center gap-1 w-full h- text-white m-2 bg-[#6f6f70]/80  focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex dark:focus:ring-[#4285F4]/55 my-40 mx-auto">
-      <h1 className="text-xl">Fat-Calculator</h1>
-      <div>
-        <label>Neck circumference (cm):</label>
-        <input
-          className="w-64"
-          type="number"
-          value={neck}
-          onChange={(e) => setNeck(e.target.value)}
-          placeholder='Enter your neck circumference in cm'
-        />
-      </div>
-      <div>
-        <label>Waist circumference (cm):</label>
-        <input
-          className="w-64"
-          type="number"
-          value={waist}
-          onChange={(e) => setWaist(e.target.value)}
-          placeholder='Enter your waist circumference in cm'
-        />
+    <div className="h-[82.5vh] flex justify-center bg-[url('/bgs/imgCalculator.png')] bg-no-repeat bg-cover backdrop-blur-sm">
+      <div className="bg-[#6f6f70]/80 rounded-lg focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 w-[30vw] m-auto text-m">
+        <div className="grid grid-cols-1 gap-1 justify-items-center h-[70vh] py-11">
+          <h1 className="text-4xl font-bold mt-4">Fat-Calculator</h1>
+          <h2 className="text-2xl font-bold text-center">
+            Enter your measures to calculate your fat percentage
+          </h2>
 
+          <div>
+            <label className="text-xl">Neck circumference (cm): </label>
+            <input
+              className="w-64 h-7 rounded-md"
+              type="number"
+              value={neck}
+              onChange={(e) => setNeck(e.target.value)}
+              placeholder="Enter your neck circumference in cm"
+            />
+          </div>
+          <div>
+            <label className="text-xl">Waist circumference (cm): </label>
+            <input
+              className="w-64 h-7 rounded-md"
+              type="number"
+              value={waist}
+              onChange={(e) => setWaist(e.target.value)}
+              placeholder="Enter your waist circumference in cm"
+            />
+          </div>
+          <div className="pl-5">
+            <label className="text-xl">Hip circumference (cm): </label>
+            <input
+              className="w-64 h-7 rounded-md"
+              type="number"
+              value={hip}
+              onChange={(e) => setHip(e.target.value)}
+              placeholder="Enter yout hip circumference in cm"
+            />
+          </div>
+          <div className="pl-[13vh]">
+            <label className="text-xl">Height (cm): </label>
+            <input
+              className="w-64 h-7 rounded-md "
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              placeholder="Enter your height in cm"
+            />
+          </div>
+
+          <div>
+            <label className="text-xl">Gender:</label>
+            <select
+              className="h-7 rounded-md"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <button
+            className="text-xl hover:text-orange-500 border-4 bg-slate-600 items-center w-40 self-center rounded-xl hover:w-60 ease-in-out duration-300"
+            onClick={calculate}
+          >
+            Calculate
+          </button>
+          <div className="text-xl">Result: {result} %</div>
+
+          <Link
+            href="/trainee/tool/tools"
+            className="mb-4 inline-block px-6  font-medium text-xs leading-tight h-[20px] w-[115px] mx-auto uppercase rounded hover:text-orange-500 transition duration-300 ease-in-out border text-center"
+          >
+            Go back
+          </Link>
+        </div>
       </div>
-      <div>
-        <label>Hip circumference (cm):</label>
-        <input
-          className="w-64"
-          type="number"
-          value={hip}
-          onChange={(e) => setHip(e.target.value)}
-          placeholder = 'Enter yout hip circumference in cm'
-        />
-     
-      </div>
-      <div>
-        <label>Height (cm):</label>
-        <input
-          className="w-64"
-          type="number"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-          placeholder='Enter your height in cm'
-        />
-      </div>
-      
-      <div>
-        <label>Gender:</label>
-        <select value={gender} onChange={(e) => setGender(e.target.value)}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-      
-      </div>
-      <button className="text-lg border-4 bg-slate-600 items-center" onClick={calculate}>Calculate</button>
-      <div className="text-lg">Result: {result} %</div>
-      <Link replace href="/guest/trainning-list">
-          <button type="button" className="inline-block px-6  font-medium text-xs leading-tight h-[70px] w-[115px] mx-auto uppercase rounded hover:text-orange-500 transition duration-300 ease-in-out">Volver</button>
-      </Link>
-    
-      
-      
     </div>
-    </div>
-    </div>
-   
   );
 };
 
