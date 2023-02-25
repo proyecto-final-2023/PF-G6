b29c2639b3a9d11112cd77549c7d9bb558d8cd1d
Hachikoi-the-creator <facka72@gmail.com>
2023-02-24 18:31:17 -0600

    post w/cookie working :D

--- client/pages/StateTest.tsx
+++ client/pages/StateTest.tsx
@@ -1,27 +1,44 @@
 import axios from "axios";
-import { useEffect } from "react";
+
+const url = "http://localhost:3001/base";
+const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRhZTM0NzBmLTViZjAtNDI0My04ZTYzLTNjMWE5MWI0OWRiYSIsImlhdCI6MTY3NzI4MjkwOCwiZXhwIjoxNjc3ODg3NzA4fQ.FYdSGY48y6OvWmUtVWVO25E3Ke9AKav6OvxLkxkiJwI`;
 
 export default function App(props: { res: string }) {
   console.log(props.res);
 
-  useEffect(() => {
-    const url = "http://localhost:3001/base";
-    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRhZTM0NzBmLTViZjAtNDI0My04ZTYzLTNjMWE5MWI0OWRiYSIsImlhdCI6MTY3NzI4MjkwOCwiZXhwIjoxNjc3ODg3NzA4fQ.FYdSGY48y6OvWmUtVWVO25E3Ke9AKav6OvxLkxkiJwI`;
+  const testGet = async () => {
+    const { data: res } = (await axios(url, {
+      headers: {
+        "x-access-token": token,
+      },
+    })) || { data: "testing" };
 
-    (async () => {
-      const { data: res } = (await axios(url, {
+    console.log(res);
+  };
+
+  const testPost = async () => {
+    const { data: res } = (await axios.post(
+      url,
+      { name: "joe" },
+      {
         headers: {
           "x-access-token": token,
         },
-      })) || { data: "testing" };
+      }
+    )) || { data: "POSTING" };
 
-      console.log(res);
-    })();
-  }, []);
+    console.log(res);
+  };
 
   return (
-    <main>
+    <main className="mt-10">
       <h1>HOLY SHIT</h1>
+      <button className="m-4 p-4 border-red-200 bg-black" onClick={testGet}>
+        GET
+      </button>
+      <button className="m-4 p-4 border-red-200 bg-black" onClick={testPost}>
+        POST
+      </button>
     </main>
   );
 }
