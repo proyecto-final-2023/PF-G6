cd1a905c8e3db87765c4d5c83fc7a863f635b0b6
Hachikoi-the-creator <facka72@gmail.com>
2023-02-24 18:23:34 -0600

    get w/cookie working

--- client/pages/StateTest.tsx
+++ client/pages/StateTest.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import useStore from "@/store";
+import axios from "axios";
+import { useEffect } from "react";
 
-export default function App() {
-  const { post, fetchPost } = useStore((state) => ({
-    post: state.post,
-    fetchPost: state.fetchPost,
-  }));
+export default function App(props: { res: string }) {
+  console.log(props.res);
+
+  useEffect(() => {
+    const url = "http://localhost:3001/base";
+    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRhZTM0NzBmLTViZjAtNDI0My04ZTYzLTNjMWE5MWI0OWRiYSIsImlhdCI6MTY3NzI4MjkwOCwiZXhwIjoxNjc3ODg3NzA4fQ.FYdSGY48y6OvWmUtVWVO25E3Ke9AKav6OvxLkxkiJwI`;
+
+    (async () => {
+      const { data: res } = (await axios(url, {
+        headers: {
+          "x-access-token": token,
+        },
+      })) || { data: "testing" };
+
+      console.log(res);
+    })();
+  }, []);
 
   return (
     <main>
-      <button onClick={fetchPost}>Fetch stuff</button>
-
-      <div>{post.id}</div>
+      <h1>HOLY SHIT</h1>
     </main>
   );
 }
