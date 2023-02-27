be14e061f3697fbe7da20cd72e6a32c8b568941a
Hachikoi-the-creator <facka72@gmail.com>
2023-02-26 21:22:50 -0600

    added deploy credentialas

--- client/pages/trainer/exercises-library.tsx
+++ client/pages/trainer/exercises-library.tsx
@@ -1,3 +1,96 @@
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+export type ResType = {
+  bodyPart: string;
+  equipement: string;
+  gifUrl: string;
+  id: number;
+  name: string;
+  target: string;
+};
+
+export default function ExercisesLibrary() {
+  const [rndExercises, setRndExercises] = useState<ResType[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios(
+          `${process.env.NEXT_PUBLIC_API_URL}/activity?name=chest&page=${currentPage}`
+        );
+        setRndExercises(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [currentPage]);
+
+  const prevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const nextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between items-center w-full pt-[115px] px-36">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Anterior
+        </button>
+        <div className="text-xl font-bold">{currentPage}</div>
+        <button
+          onClick={nextPage}
+          disabled={rndExercises.length === 0}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Siguiente
+        </button>
+      </div>
+      <div className="flex flex-wrap gap-20">
+        {rndExercises.slice(0, 30).map((ex) => (
+          <Link
+            href={"/trainer/exercise/" + ex.id}
+            replace
+            key={ex.id}
+            className="w-1/4 rounded-sm bg-slate-500"
+          >
+            <p>Body part {ex.bodyPart}</p>
+            <Image src={ex.gifUrl} alt="" width={300} height={300} />
+          </Link>
+        ))}
+        <div className="flex justify-between items-center w-full mt-10 px-36">
+          <button
+            onClick={prevPage}
+            disabled={currentPage === 1}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Anterior
+          </button>
+          <div className="text-xl font-bold">{currentPage}</div>
+          <button
+            onClick={nextPage}
+            disabled={rndExercises.length === 0}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Siguiente
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 // import axios from "axios";
 // import Image from "next/image";
 // import Link from "next/link";
@@ -39,98 +132,3 @@
 //     </div>
 //   );
 // }
-
-import axios from "axios";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-
-export type ResType = {
-  bodyPart: string;
-  equipement: string;
-  gifUrl: string;
-  id: number;
-  name: string;
-  target: string;
-};
-
-export default function ExercisesLibrary() {
-  const [rndExercises, setRndExercises] = useState<ResType[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(
-          `http://localhost:3001/activity/filter/target/pectorals?page=${currentPage}`
-        );
-        setRndExercises(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [currentPage]);
-
-  const prevPage = () => {
-    setCurrentPage((prev) => prev - 1);
-  };
-
-  const nextPage = () => {
-    setCurrentPage((prev) => prev + 1);
-  };
-
-  return (
-      <div>
-        <div className="flex justify-between items-center w-full pt-[115px] px-36">
-        <button
-          onClick={prevPage}
-          disabled={currentPage === 1}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Anterior
-        </button>
-        <div className="text-xl font-bold">{currentPage}</div>
-        <button
-          onClick={nextPage}
-          disabled={rndExercises.length === 0}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Siguiente
-        </button>
-      </div>
-        <div className="flex flex-wrap gap-20">
-      {rndExercises.slice(0, 30).map((ex) => (
-        <Link
-          href={"/trainer/exercise/" + ex.id}
-          replace
-          key={ex.id}
-          className="w-1/4 rounded-sm bg-slate-500"
-        >
-          <p>Body part {ex.bodyPart}</p>
-          <Image src={ex.gifUrl} alt="" width={300} height={300} />
-        </Link>
-      ))}
-      <div className="flex justify-between items-center w-full mt-10 px-36">
-        <button
-          onClick={prevPage}
-          disabled={currentPage === 1}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Anterior
-        </button>
-        <div className="text-xl font-bold">{currentPage}</div>
-        <button
-          onClick={nextPage}
-          disabled={rndExercises.length === 0}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Siguiente
-        </button>
-      </div>
-    </div>
-      </div>
-    
-  );
-}
--- client/utils/ssrProtection.ts
+++ client/utils/ssrProtection.ts
@@ -2,13 +2,14 @@ import axios from "axios";
 
 export default async () => {
   // Check if the user is authenticated by calling an API route
-  const { data } = await axios("http://localhost:3000/api/auth");
+
+  const { data } = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/auth`);
 
   // If the user is not authenticated, redirect them to the login page
   if (!data.auth) {
     return {
       redirect: {
-        destination: "http://localhost:3000",
+        destination: `${process.env.NEXT_PUBLIC_FRONT_DEPLOY}/login`,
         permanent: false,
       },
     };
