baf8b5b15d9554cf211ac7fb5d52a11469336d53
Hachikoi-the-creator <facka72@gmail.com>
2023-02-26 21:46:56 -0600

    added food-library

---
+++ client/pages/trainer/food/[id].tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { FoodResType } from "../food-library";
+
+export default function FoodDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+
+  const [foodData, setFoodData] = useState<FoodResType>();
+
+  const goBackHandler = () => {
+    router.replace("/trainer/exercises-library");
+  };
+
+  useEffect(() => {
+    axios(`${process.env.NEXT_PUBLIC_API_URL}/aliment/${id}`).then(({ data }) =>
+      setFoodData(data)
+    );
+  }, []);
+
+  return (
+    <div className="mt-20">
+      <p>Name: {foodData?.description}</p>
+      <p>
+        Energy: {foodData?.energyAmount} {foodData?.energylUnit}.
+      </p>
+      <p>
+        Cholesterol: {foodData?.cholesterolAmount} {foodData?.cholesterolUnit}.
+      </p>
+      <p>
+        Protein: {foodData?.proteinAmount} {foodData?.proteinUnit}.
+      </p>
+      <button
+        className="bg-red-200 text-cyan-900 p-3 rounded-sm mt-10"
+        onClick={goBackHandler}
+      >
+        Go back
+      </button>
+    </div>
+  );
+}
