92f1af3aac106e9c7aefd01110ce31f6b18b81c1
JavGarb <olguiyjavi@gmail.com>
2023-02-28 20:28:32 -0500

    Graphic ready

---
+++ client/components/adminDashboard/GraficUTT.tsx
@@ -0,0 +1,64 @@
+import React, { useMemo } from "react";
+import Chart from 'chart.js/auto';
+import {Doughnut} from 'react-chartjs-2';
+
+import {
+  DoughnutController,
+  ArcElement,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  Filler,
+} from "chart.js";
+
+
+
+Chart.register(
+  DoughnutController,
+  ArcElement,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  Filler
+);
+
+const scores = [100, 200, 300, 400];
+const labels = ["uno", "dos", "tres"];
+
+function GraficUTT() {
+  const data = useMemo(
+    () => ({
+      datasets: [
+        {
+          label: 'mis datos',
+          data: scores,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.6)",
+            "rgba(54, 162, 235, 0.6)",
+            "rgba(255, 206, 86, 0.6)",
+            "rgba(75, 192, 192, 0.6)",
+          ],
+        },
+      ],
+      labels,
+     
+    }),
+    [labels, scores]
+  );
+
+  return (
+    <div>
+      <Doughnut data={data} options={{ responsive: true }} className="inline-block w-1/3 p-4"/>
+    </div>
+  );
+}
+
+export default GraficUTT;
---
+++ client/components/adminDashboard/GrapContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GraficUTT from './GraficUTT'
+
+function GrapContainer() {
+  return (
+    <div className="flex justify-between m-20">
+        <GraficUTT />
+        <GraficUTT />
+        <GraficUTT />
+    </div>
+  )
+}
+
+export default GrapContainer
