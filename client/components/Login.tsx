20e69a1b2a3d55772a28cccc9bb9e2ef764db51c
OssFit <oalatrista@gmail.com>
2023-03-02 12:57:41 -0500

    update

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
-import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, AuthProvider, Auth, User } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, AuthProvider} from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Router from "next/router";
 import axios from 'axios'
