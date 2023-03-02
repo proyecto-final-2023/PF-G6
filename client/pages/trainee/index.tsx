26ddc3b6a4f06c2f19abfc09a8c580119ee7cfb6
jpquinteros <jp.quinterosaraos@gmail.com>
2023-03-02 02:01:48 -0300

    Calendario diario y por agenda +extern profilepic + name

--- client/pages/trainee/index.tsx
+++ client/pages/trainee/index.tsx
@@ -17,6 +17,9 @@ import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 
 // type newEvent = {
 //   title: string;
@@ -26,7 +29,11 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function Index() {
   // const [progress, setProgress] = useState("0");
-  const percentage = 80;
+  const percentage = 70;
+
+  const [user, setUser] = useAuthState(auth);
+  const photo = user?.photoURL;
+  const name = user?.displayName;
 
   const locales = {
     "en-US": require("date-fns/locale/en-US"),
@@ -48,7 +55,7 @@ export default function Index() {
       end: new Date(2023, 2, 10),
     },
     {
-      title: "lifting",
+      title: "Lifting",
       allDay: true,
       start: new Date(2023, 2, 24),
       end: new Date(2023, 2, 24),
@@ -81,22 +88,20 @@ export default function Index() {
   }
 
   return (
-    <>
-      <div className="flex justify-around align-middle h-[30vh]">
-        <div className="my-auto -mx-40">
-          <CircularProgressbar
-            className="w-40 h-40 my-20"
-            value={percentage}
-            text={`${percentage}%`}
-          />
-        </div>
-        <h1 className="my-auto">
-          <Link href="">
-            <p>My profile</p>
-          </Link>
-        </h1>
-        <div>{/* <Image src={myImage} alt="" className="bg-cover" /> */}</div>
+    <div className="relative">
+      <CircularProgressbar
+        className="absolute top-0 left-0 w-40 h-40"
+        value={percentage}
+        text={`${percentage}%`}
+      />
+
+      <div className="flex flex-col h-[20-vh] align-bottom">
+        <img className="rounded-full w-40 h-40" src={photo} alt=""></img>
+        <h1 className="text-3xl">{name}</h1>
       </div>
+
+      <div>{/* <Image src={myImage} alt="" className="bg-cover" /> */}</div>
+
       <div>
         <h1 className="flex justify-center text-5xl pb-4">Schedule</h1>
         <h2 className="flex justify-center text-2xl pb-4">Add New Event</h2>
@@ -134,62 +139,19 @@ export default function Index() {
         </div>
         <div className="bg-gradient-to-r from-gray-800 via-orange-500 to-gray-800">
           <div className="bg-[url('/bgs/logoblack.png')] bg-contain bg-no-repeat bg-center ">
-          <Calendar
-            localizer={localizer}
-            events={allEvents}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500, margin: "50px", backgroundColor:'transparent', }}
-            defaultView="day"
-            views={{month: false, week: false, day: true, agenda: false}}   
-            className='text-3xl text-white'
-          />
+            <Calendar
+              localizer={localizer}
+              events={allEvents}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500, margin: "50px" }}
+              defaultView="agenda"
+              views={{ month: false, week: false, day: true, agenda: true }}
+              className="text-3xl text-white"
+            />
           </div>
         </div>
       </div>
-
-      {/* <div>
-        <div>
-          <h1 className="flex justify-center text-5xl">Schedule</h1>
-          <h2 className="flex justify-center text-2xl">Add new Event</h2>
-          <div className="flex justify-center ml-[10vw] gap-3">
-            <div className="flex">
-              <input
-                type="text"
-                placeholder="Add title"
-                value={newEvent.title}
-                onChange={(e) =>
-                  setNewEvent({ ...newEvent, title: e.target.value })
-                }
-                className="w-[20vw] mr-2 rounded-md"
-              />
-              <DatePicker
-                placeholderText="Start Date"
-                selected={newEvent.start}
-                onChange={(start) => setNewEvent({ ...newEvent, start: start })}
-                className="w-[5vw] rounded-md mx-auto"
-              />
-              <DatePicker
-                placeholderText="End Date"
-                selected={newEvent.start}
-                onChange={(end) => setNewEvent({ ...newEvent, end: end })}
-                className="w-[5vw] rounded-md mx-auto"
-              />
-            </div>
-          </div>
-          <div className="mx-auto my-8 text-center hover:text-orange-500 border-2 bg-slate-600 w-20 rounded-xl hover:w-40 ease-in-out duration-300">
-            <button onClick={handleAddEvent}>Add Event</button>
-          </div>
-          <Calendar
-            localizer={localizer}
-            events={allEvents}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500, margin: "50px" }}
-            className="bg-orange-400 text-lg inline-block"
-          />
-        </div>
-      </div> */}
-    </>
+    </div>
   );
 }
