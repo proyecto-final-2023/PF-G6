0b6db3789cfaae92d485830b47fb92987092556c
Hachikoi-the-creator <facka72@gmail.com>
2023-03-03 19:16:55 -0600

    before mirro merge

---
+++ client/components/adminDashboard/memberships/MembershipsContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * todo: list :D
+ * handles backend call to fetch memberships
+ * handles search (may add later)
+ */
+export default function MembershipsContainer({ page }: { page: number }) {
+  return <div>MembershipsContainer</div>;
+}
