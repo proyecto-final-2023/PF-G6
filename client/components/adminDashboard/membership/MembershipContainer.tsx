197634931b8482a887619bd2cb67fcb3bcdb5e75
Hachikoi-the-creator <facka72@gmail.com>
2023-03-06 11:51:30 -0600

    deploy-fix v3

--- client/components/adminDashboard/membership/MembershipContainer.tsx
+++ client/components/adminDashboard/membership/MembershipContainer.tsx
@@ -7,11 +7,10 @@ import { useEffect, useState } from "react";
  * handles backend call to fetch memberships
  * handles search (may add later)
  */
-type Membership = {
-  
+type Membership = {};
+
 export default function MembershipsContainer() {
   const [page, setPage] = useState(1);
-  const [memshi, setMemshi] = useState([])
 
   const nextPage = () => {
     setPage((prev) => prev + 1);
@@ -21,21 +20,10 @@ export default function MembershipsContainer() {
     setPage((prev) => prev - 1);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data }: { data: [] } = await axios(
-        `${process.env.NEXT_PUBLIC_API_URL}/plans/trainers?page=${page}`
-      );
-      setPlans(data);
-    };
-    fetchData();
-  }, [page]);
-
   return (
     <div>
       <h2 className="text-xl text-center">Memberships Container</h2>
       <NavigationBtns currentPage={page} {...{ nextPage }} {...{ prevPage }} />
-      <p></p>
     </div>
   );
 }
