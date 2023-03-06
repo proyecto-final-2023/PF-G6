a091beb6322cec628fe30aba37efac199f36cfd0
Hachikoi-the-creator <facka72@gmail.com>
2023-03-06 00:37:56 -0600

    ctrl-c FTW

---
+++ client/utils/fileUpload.ts
@@ -0,0 +1,30 @@
+// Documemtation about of how to upload Images in cloudinary
+// https://cloudinary.com/documentation/image_upload_api_reference
+// cloud name: dfixfnldt
+// preset name: basic-img-preset
+
+const cloud_name = process.env.REACT_APP_CLOUD_NAME;
+const preset = process.env.REACT_APP_PRESET;
+
+export const fileUpload = async (file: File): Promise<string | null> => {
+  const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`;
+
+  const formData = new FormData();
+  formData.append("upload_preset", `${preset}`);
+  formData.append("file", file);
+
+  try {
+    const res = await fetch(cloudinaryUrl, {
+      method: "POST",
+      body: formData
+    });
+
+    if (!res.ok) return null;
+
+    const data = await res.json();
+    return data.secure_url;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
