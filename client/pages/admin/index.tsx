ea2c45d3c1c33a09adb9556d7a3949854d7f2488
Hachikoi-the-creator <facka72@gmail.com>
2023-03-08 20:20:27 -0600

    protected admin

--- client/pages/admin/index.tsx
+++ client/pages/admin/index.tsx
@@ -1,21 +1,33 @@
+import cookie from "cookie";
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
 import DataViewContainer from "@/components/adminDashboard/DataViewContainer";
 import GraphContainer from "@/components/adminDashboard/GraphContainer";
 import OptionMenu from "@/components/adminDashboard/OptionMenu";
-import CentralContainer from "@/components/adminDashboard/CentralContainer";
+// import CentralContainer from "@/components/adminDashboard/CentralContainer";
 import EditableTablePlans from "@/components/adminDashboard/EditablePlans";
-import { useState } from "react";
 import EditableTableUser from "@/components/adminDashboard/EditableUser";
 import EditableComments from "@/components/adminDashboard/EditableComments";
 import EditableTableTrainees from "@/components/adminDashboard/EditableTrainees";
 import EditableTableTrainer from "@/components/adminDashboard/EditableTrainers";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-export default function AdminIndex() {
+export default function AdminIndex(props: { isAdmin: boolean }) {
+  const router = useRouter();
+  if (!props.isAdmin) router.replace("/home");
+  else console.log("Hewo sexy sir Admin uwu~");
+
+  // const [token, setToken] = useState("");
+  // const ticcAss = getCookie("token");
+  // console.log(ticcAss, "props.myCookie");
+
   const [option, setOption] = useState("");
 
   const changeOption = (option: string) => {
     setOption(option);
   };
-  
+
   // return (
   //   <div className="mt-20">
   //     <DataViewContainer />
@@ -111,3 +123,32 @@ export default function AdminIndex() {
       );
   }
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Get cookie then api call to check if user is admin
+  const { req } = context;
+  const cookies = cookie.parse(req.headers.cookie || "1313");
+  const myCookie = cookies["token"];
+  let isAdmin = false;
+
+  try {
+    const res: { data: { role: string } } = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/perfil`,
+      {},
+      {
+        headers: {
+          "x-access-token": myCookie
+        }
+      }
+    );
+    isAdmin = res.data.role === "admin";
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {
+    props: {
+      isAdmin
+    }
+  };
+};
