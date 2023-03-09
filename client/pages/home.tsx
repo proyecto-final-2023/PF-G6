5ae07f1d62db4a98d3f8332dc85dcf38e0af5d13
Alexsilverr <alexsilverr2390@gmail.com>
2023-03-09 01:05:42 -0300

    alex-css-este-si

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -5,8 +5,7 @@ import SwiperCarousel from "@/components/Carousel/SwiperCarousel";
 import Link from "next/link";
 import WithPrivateRouter from "@/components/WithPrivateRoute";
 
- function Home() {
-
+function Home() {
   return (
     <>
       <div className="bg-[url('/tail-imgs/gym-bg.jpg')] bg-no-repeat bg-cover bg-bottom bg-fixed  backdrop-blur-sm">
@@ -30,43 +29,46 @@ import WithPrivateRouter from "@/components/WithPrivateRoute";
         </div>
       </div>
       <div className="text-center py-20 ">
-     
-        
-<h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Find the Best Plans For You!</h1>
-<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Fit-U we focus on tools where  value and drive growth.</p>
-
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl ">
+          Find the Best Plans For You!
+        </h1>
+        <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
+          Here at Fit-U we focus on tools where value and drive growth.
+        </p>
 
         <div className="  flex flex-row gap-20  m-10 h-60 justify-center align-items:center">
-        <Link replace href="/plans">
-          <div className=" bg-black h-60 rounded-lg animate-pulse   shadow-yellow-900/50 bg-[url('/tail-imgs/1zLe.gif')] bg-no-repeat bg-cover bg-bottom   max-w-sm overflow-hidden shadow-lg">
-            <div className="px-6 py-4 "> 
-              <h1 className="flex items-center text-5xl font-extrabold dark:text-white">Trainer<span className="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">Plans</span></h1>
+          <Link replace href="/plans">
+            <div className=" bg-black h-60 rounded-lg animate-pulse   shadow-yellow-900/50 bg-[url('/tail-imgs/1zLe.gif')] bg-no-repeat bg-cover bg-bottom   max-w-sm overflow-hidden shadow-lg">
+              <div className="px-6 py-4 ">
+                <h1 className="flex items-center text-5xl font-extrabold text-white">
+                  Trainer
+                  <span className="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded ml-2">
+                    Plans
+                  </span>
+                </h1>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link replace href="/plansTrainee">
-          <div className=" bg-gray-800 max-w-sm h-60 rounded-lg animate-pulse  shadow-yellow-900/50  bg-[url('/tail-imgs/1zLe.gif')] bg-no-repeat bg-cover bg-bottom  overflow-hidden shadow-lg">
-            <div className="px-6 py-4">
-              
-              <h1 className="flex items-center text-5xl font-extrabold dark:text-white">Trainee<span className="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">Plans</span></h1>
+          </Link>
+          <Link replace href="/plansTrainee">
+            <div className=" bg-gray-800 max-w-sm h-60 rounded-lg animate-pulse  shadow-yellow-900/50  bg-[url('/tail-imgs/1zLe.gif')] bg-no-repeat bg-cover bg-bottom  overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <h1 className="flex items-center text-5xl font-extrabold :text-white">
+                  Trainee
+                  <span className="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded ml-2">
+                    Plans
+                  </span>
+                </h1>
+              </div>
             </div>
-          </div>
-        </Link>
-     
-        </div >
-      <div className="bg-[url('/tail-imgs/gym-bg.jpg')] bg-no-repeat bg-cover bg-bottom bg-fixed  h-[100vh]">
-      
-      
-     
-
-
-
-      </div>
-      <SwiperCarousel slidesArr={homeSlides} />
+          </Link>
+        </div>
+        <div className="bg-[url('/tail-imgs/gym-bg.jpg')] bg-no-repeat bg-cover bg-bottom  h-[100vh]">
+        <SwiperCarousel slidesArr={homeSlides} />
+        </div>
        
       </div>
     </>
   );
 }
 
-export default WithPrivateRouter(Home)
+export default WithPrivateRouter(Home);
